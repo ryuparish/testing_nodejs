d1c8d83e23225262781e7717fc8bd10607d859e4
ryuparish <ryuparish1115@gmail.com>
2021-11-19 01:10:37 -0800

    Added kafka dummy interval messager and instructions

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
 var http = require('http');
 var fs = require('fs');
+// import the `Kafka` instance from the kafkajs library
 const port = 8000;
 const hostname = '127.0.0.1';
+// import the `Kafka` instance from the kafkajs library
+const { Kafka } = require("kafkajs")
+
+// the client ID lets kafka know who's producing the messages
+const clientId = "kafkaTestClient"
+// we can define the list of brokers in the cluster
+const brokers = ["localhost:9092"]
+// this is the topic to which we want to write messages
+const topic = "testTopic"
+
+// initialize a new kafka client and initialize a producer from it
+const kafka = new Kafka({ clientId, brokers })
+const producer = kafka.producer()
+
+// we define an async function that writes a new message each second
+const produce = async () => {
+	await producer.connect()
+	let i = 0
+
+	// after the produce has connected, we start an interval timer
+	setInterval(async () => {
+		try {
+			// send a message to the configured topic with
+			// the key and value formed from the current value of `i`
+			await producer.send({
+				topic,
+				messages: [
+					{
+						key: String(i),
+						value: "this is message " + i,
+					},
+				],
+			})
+
+			// if the message is written successfully, log it and increment `i`
+			console.log("writes: ", i)
+			i++
+		} catch (err) {
+			console.error("could not write message " + err)
+		}
+	}, 1000)
+}
+
+module.exports = produce
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
@@ -14,6 +59,10 @@ function onRequest(request, response) {
         }
         response.end();
     });
+    produce().catch((err) => {
+            console.error("error in producer: ", err)
+    })
+
 }
 
 const server = http.createServer(onRequest).listen(port, hostname, () => {
